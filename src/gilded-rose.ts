f8c80da412b37dfbff5e3a5f805c99eb6dec370f
Miroslav Vrana <mvrana@csas.cz>
2024-01-11 16:07:17 +0100

    refactor: phase 11

--- src/gilded-rose.ts
+++ src/gilded-rose.ts
@@ -15,17 +15,16 @@ export class GildedRose {
         this.items.forEach((item) => {
             if (item.name == 'Aged Brie') {
                 this.adjustQuality(item, 1);
-
             } else if(item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                if (item.quality < 50) {
-                    item.quality++;
-                    if (item.sellIn < 11) {
-                        this.adjustQuality(item, 1);
-                    }
-                    if (item.sellIn < 6) {
-                        this.adjustQuality(item, 1);
-                    }
+                let adjustment: number;
+                if (item.sellIn <= 5) {
+                    adjustment = 3;
+                } else if (item.sellIn <= 10) {
+                    adjustment = 2;
+                } else {
+                    adjustment = 1;
                 }
+                this.adjustQuality(item, adjustment);
             } else {
                 if (item.name != 'Sulfuras, Hand of Ragnaros') {
                     this.adjustQuality(item, -1);
